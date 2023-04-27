b6d34aa3f573fa28e85fe72a26448fcfb4b08d98
Bob Tanner <tanner@real-time.com>
2023-04-27 18:31:58 -0500

    Remove $ from $url

--- tools/data.ps1
+++ tools/data.ps1
@@ -1,5 +1,5 @@
 @{
-    $url = 'https://1243.3cx.cloud/webclient/api/app/windows'
+    url = 'https://1243.3cx.cloud/webclient/api/app/windows'
     checksum = '0031836e535ec47599e42070028c73fada617b2a9fd597034de76f43599162a7'
     checksumType = 'sha256'
 }
