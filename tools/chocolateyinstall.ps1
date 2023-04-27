fd3c61f313a534b9be452625df9b63d3ab28b284
Bob Tanner <tanner@real-time.com>
2023-04-27 18:12:07 -0500

    Reviving this package
      * New release v18.11.1213 (Steven Orvis <sorvis@kencove.com>)
      * Add pre-commit hooks
      * Put data that changes into data.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
+
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://1243.3cx.cloud/webclient/api/app/windows'
 
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     unzipLocation  = $toolsDir
     fileType       = 'msi'
-    url            = $url
     softwareName   = '3CX*'
-    checksum       = '0031836e535ec47599e42070028c73fada617b2a9fd597034de76f43599162a7'
-    checksumType   = 'sha256'
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
     validExitCodes = @(0, 3010, 1641)
+
+    url             = $data.url
+    checksum        = $data.checksum
+    checksumType    = $data.checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
